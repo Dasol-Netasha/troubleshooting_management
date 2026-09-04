51b7c409b78f6e176d9f0139f88d9470da692555
Dasol <dskwon@enscapte.co.kr>
2026-09-04 09:17:23 +0900

    [ADD] detail page에서 수정화면으로 넘어가는 버튼 추가

--- frontend/src/views/pages/DetailPage.vue
+++ frontend/src/views/pages/DetailPage.vue
@@ -1,9 +1,11 @@
 <script setup>
-import { useRouter } from 'vue-router'
+import { useRoute, useRouter } from 'vue-router'
 import DetailDataTemplate from '@/views/templates/detail-page/DetailDataTemplate.vue'
 import DetailDataImagesTEmplate from '@/views/templates/detail-page/DetailDataImagesTEmplate.vue'
 import BackNavIconBtn from '@/components/molecules/buttons/BackNavIconBtn.vue'
+import EditBtn from '@/views/organisms/list-page/EditBtn.vue'
 
+const route = useRoute()
 const router = useRouter()
 
 const goToList = () => {
@@ -16,6 +18,7 @@ const goToList = () => {
     <div class="flex items-center gap-2">
       <BackNavIconBtn @click="goToList" />
       <h1 class="text-2xl font-semibold text-slate-900">Detail Page</h1>
+      <EditBtn :issue-id="route.params.issueId" />
     </div>
 
     <DetailDataTemplate />
